8a14dc9c0789cf4e434eef3f9263fb03f7e4ea15
Martin Zurowietz <martin@zurowietz.de>
2025-07-21 12:10:12 +0200

    Handle case where registration is disabled but user exists

--- tests/Http/Controllers/HaaiControllerTest.php
+++ tests/Http/Controllers/HaaiControllerTest.php
@@ -38,7 +38,25 @@ class HaaiControllerTest extends TestCase
         Socialite::shouldReceive('driver->user')->andReturn($user);
 
         $this->get('auth/haai/callback')
-            ->assertInvalid(['haai-id'])
+            ->assertInvalid([
+                'haai-id' => 'The user does not exist and new registrations are disabled.',
+            ])
+            ->assertRedirectToRoute('login');
+    }
+
+    public function testCallbackNewUserRegistrationDisabledUserExists()
+    {
+        User::factory()->create(['email' => 'joe@example.com']);
+        config(['biigle.user_registration' => false]);
+        $user = new SocialiteUser;
+        $user->email = 'joe@example.com';
+        $user->setToken('mytoken');
+        Socialite::shouldReceive('driver->user')->andReturn($user);
+
+        $this->get('auth/haai/callback')
+            ->assertInvalid([
+                'haai-id' => 'The email has already been taken. You can connect your existing account to Helmholtz AAI in the account authorization settings.',
+            ])
             ->assertRedirectToRoute('login');
     }
 
